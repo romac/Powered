836e1a7ccdc473b54794572d40428315b504a51e
Romain Ruetschi <romain.ruetschi@gmail.com>
2009-10-05 09:39:52 +0200

    Raised minmal required version for extbase and fluid to 0.9.9.

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,8 +37,8 @@ $EM_CONF[ $_EXTKEY ] = array(
         'depends'   => array(
             'php'     => '5.2.0-0.0.0',
             'typo3'   => '4.3.dev-4.3.99',
-            'extbase' => '0.9.0-1.0.0',
-            'fluid'   => '0.1.0-0.2.0',
+            'extbase' => '0.9.9-1.0.0',
+            'fluid'   => '0.9.9-1.0.0',
         ),
         'conflicts' => array(),
         'suggests'  => array(
