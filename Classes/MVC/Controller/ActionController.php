ccf0f5a199b56f5236b32cff819dbca756ce8e52
Romain Ruetschi <romain.ruetschi@gmail.com>
2010-05-25 13:36:27 +0200

    Reformatted some code.

--- Classes/MVC/Controller/ActionController.php
+++ Classes/MVC/Controller/ActionController.php
@@ -52,11 +52,71 @@ abstract class Tx_Powered_MVC_Controller_ActionController extends Tx_Extbase_MVC
      *
      * @var Tx_Powered_Helper_RepositoryContainer
      */
-    protected $repositoryContainer = NULL;
+    protected $repositoryContainer              = NULL;
     
+    /**
+     * Public Resources Web Path
+     *
+     * @var string
+     */
+    protected $publicResourcesWebPath           = '';
+    
+    /**
+     * Public Resources Path
+     *
+     * @var string
+     */
+    protected $publicResourcesPath              = '';
+    
+    /**
+     * Extension name
+     *
+     * @var string
+     */
+    protected $lowerCasedExtensionName          = '';
+    
+    /**
+     * View adapter class name.
+     *
+     * @var string
+     */
+    protected $viewAdapterClassName             = 'Tx_Powered_MVC_View_ViewAdapter';
+    
+    /**
+     * Repository container class name.
+     *
+     * @var string
+     */
+    protected $repositoryContainerClassName     = 'Tx_Powered_Helper_RepositoryContainer';
+    
+    /**
+     * This method is called just before the action method is launched.
+     * It can be used to do generic stuff that all actions need.
+     *
+     * @return void
+     * @author Romain Ruetschi <romain.ruetschi@gmail.com>
+     */
     public function initializeAction()
     {
+        parent::initializeAction();
+        
         $this->initializeHelpers();
+        $this->initializeResourcesPaths();
+    }
+    
+    /**
+     * Initialize the resources paths.
+     *
+     * @return void
+     * @author Romain Ruetschi <romain.ruetschi@gmail.com>
+     */
+    protected function initializeResourcesPaths()
+    {
+        $extKey                       = $this->getLowerCasedExtensionName();
+        $relPath                      = substr( t3lib_extMgm::extPath( $extKey ), strlen( PATH_site ) );
+        
+        $this->publicResourcesWebPath = $relPath  . 'Resources/Public/';
+        $this->publicResourcesPath    = t3lib_extMgm::extPath( $extKey ) . 'Resources/Public/';
     }
     
     /**
@@ -69,7 +129,7 @@ abstract class Tx_Powered_MVC_Controller_ActionController extends Tx_Extbase_MVC
     {
         // Get a repository container.
         $this->repositoryContainer = t3lib_div::makeInstance(
-            'Tx_Powered_Helper_RepositoryContainer',
+            $this->repositoryContainerClassName,
             $this->buildControllerContext()
         );
     }
@@ -86,9 +146,95 @@ abstract class Tx_Powered_MVC_Controller_ActionController extends Tx_Extbase_MVC
         parent::initializeView( $view );
         
         $this->view = t3lib_div::makeInstance(
-            'Tx_Powered_MVC_View_ViewAdapter',
-            $view
+            $this->viewAdapterClassName,
+            $this->view
         );
+    }
+    
+    /**
+     * Get the loweredcased extension's name this controller belongs to.
+     *
+     * @return string The lowercased underscored extension name.
+     * @author Romain Ruetschi <romain.ruetschi@gmail.com>
+     */
+    protected function getLowerCasedExtensionName()
+    {
+        if( !$this->lowerCasedExtensionName ) {
+            
+            $this->lowerCasedExtensionName = Tx_Extbase_Utility_Extension::convertCamelCaseToLowerCaseUnderscored(
+                $this->request->getControllerExtensionName()
+            );
+        }
+        
+        return $this->lowerCasedExtensionName;
+    }
+    
+    /**
+     * Include a stylesheet corresponding to the current action.
+     * File is supposed to be found at:
+     * EXT:{extKey}/Resources/Public/CSS/{controller}/{lowerCasedAction}.css
+     *
+     * @return boolean True if the file was found and included, false otherwise.
+     * @author Romain Ruetschi <romain.ruetschi@gmail.com>
+     */
+    protected function addStyleSheetForCurrentAction()
+    {
+        $controller     = $this->request->getControllerName();
+        $action         = $this->request->getControllerActionName();
+        $controllerDir  = $this->publicResourcesPath . 'CSS/' . $controller . '/';
+        $actionFile     = strtolower( $action ) . '.css';
+        
+        if( !is_dir( $controllerDir ) ) {
+            
+            return FALSE;
+        }
+        
+        if( !file_exists( $controllerDir . $actionFile ) ) {
+            
+            return FALSE;
+        }
+        
+        $html = '<link href="' . $this->publicResourcesWebPath
+              . 'CSS/' . $controller . '/' . $actionFile
+              . '" rel="stylesheet" type="text/css" media="screen" charset="utf-8" />';
+        
+        $this->response->addAdditionalHeaderData( $html );
+        
+        return TRUE;
+    }
+    
+    /**
+     * Include a JavaScript file corresponding to the current action.
+     * File is supposed to be found at:
+     * EXT:{extKey}/Resources/Public/JavaScripts/{controller}/{action}.js
+     *
+     * @return boolean True if the file was found and included, false otherwise.
+     * @author Romain Ruetschi <romain.ruetschi@gmail.com>
+     */
+    protected function addJavaScriptForCurrentAction()
+    {
+        $controller     = $this->request->getControllerName();
+        $action         = $this->request->getControllerActionName();
+        $controllerDir  = $this->publicResourcesPath . 'JavaScripts/' . $controller . '/';
+        $actionFile     = strtolower( $action ) . '.js';
+        
+        if( !is_dir( $controllerDir ) ) {
+            
+            return FALSE;
+        }
+        
+        if( !file_exists( $controllerDir . $actionFile ) ) {
+            
+            return FALSE;
+        }
+        
+        $html = '<script src="' . $this->publicResourcesWebPath
+              . 'JavaScripts/' . $controller . '/' . $actionFile
+              . '" type="text/javascript"></script>';
+        
+        $this->response->addAdditionalHeaderData( $html );
+        
+        return TRUE;
     }
     
     /**
--- ext_localconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if( !defined( 'TYPO3_MODE' ) ) {
-    
-    die( 'Access denied.' );
-}
-
-// $TYPO3_CONF_VARS[ 'SC_OPTIONS' ][ 't3lib/class.t3lib_tcemain.php' ][ 'clearCachePostProc' ][] = 'EXT:powered/Classes/Utility/CacheProvider.php:user_txPoweredUtilityCacheProvider_clearCache';
